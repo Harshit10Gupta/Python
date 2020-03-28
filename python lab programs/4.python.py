e0dbbddf7c4fef56637831ca3ab9f7caaf4664c6
Harshit10Gupta <59639403+Harshit10Gupta@users.noreply.github.com>
2020-03-28 18:15:52 +0530

    Add files via upload

---
+++ python lab programs/4.python.py
@@ -0,0 +1,12 @@
+#4.Write a python program to swap two numbers using temporary variable.
+a=10
+b=20
+print("Before swapping")
+print(a)
+print(b)
+c=a
+a=b
+b=c
+print("After swapping")
+print(a)
+print(b)
---
+++ python lab programs/5.python.py
@@ -0,0 +1,12 @@
+#5.Write a python program to swap two numbers without using temporary variable.
+a=20
+b=10
+print("Before swapping")
+print(a)
+print(b)
+a=a+b
+b=a-b
+a=a-b
+print("After swapping")
+print(a)
+print(b)
